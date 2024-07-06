00e1f54640ccb12cbf96803f0def08b7691369a9
handoko <srabutdotcom@gmail.com>
2024-07-06 07:15:50 +0700

    small notes in mod.js

--- mod.js
+++ mod.js
@@ -3,3 +3,10 @@ export { Handshake } from './src/handshake.js';
 
 
 //`esbuild ./mod.js --bundle --format=esm --target=esnext --outfile=./dist/tls13parser.js`
+/**!SECTION
+ * 1. git init
+ * 2. git remote add origin [URL of github repository]
+ * 3. git add .
+ * 4. git commit -m 'commit'
+ * 5. git push origin master --force 
+ */
